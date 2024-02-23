b4795aef0615e5769bb6715f3c332e1af5a0116c
David <david@tellus1.com>
2024-02-23 13:37:45 -0600

    Ready!
    nonlinearity_script.py produces FITS files in the Output directory

--- emccd_detect/nonlinearity_script.py
+++ emccd_detect/nonlinearity_script.py
@@ -27,7 +27,7 @@ def nonlinearityFactor(c_init,em_gain):
     from scipy.interpolate import interp1d
     
     # Specify filename containing nonlinearity data
-    nonlin_df =  pd.read_csv('nonlin_array.csv')
+    nonlin_df =  pd.read_csv('nonlin_estimate.csv')
     
     # Extract the magnitudes covered, the gains covered
     marks = np.array(nonlin_df.iloc[1:,0])
@@ -47,61 +47,40 @@ def nonlinearityFactor(c_init,em_gain):
     nl = nl_flat.reshape(c_init.shape)
     c_final = np.multiply(c_init,nl)
     return nl, c_final
-    
-    
-    
-    
-def _nonlin(dn_init, em_gain, nonlin_DNs, nonlin_emgains, nonlin_vals):
-    from scipy.interpolate import griddata
-    em_gain_log = np.log10(em_gain)
-    log_nonlin_emgains = np.log10(nonlin_emgains)
-    emgainLogMesh, dnMesh = np.meshgrid(log_nonlin_emgains,nonlin_DNs)
-    # final_DN = griddata((emgainMesh.flatten(), dnMesh.flatten()), nonlin_vals.flatten(),
-    #                     (em_gain,dn_init), method='linear')
-    final_DN = griddata((emgainLogMesh, dnMesh), nonlin_vals,(em_gain_log,dn_init), method='linear')
-    return final_DN
 
 if __name__ == '__main__':
-    exposureTimes = 2 #np.array([2,5,7])
-    numFrames = 1 #5
+    exposureTimes = np.array([2,5,7],dtype=int) #np.array([2,5,7])
+    numFrames = 48 #5 #35
+    # Set up some inputs here
+    here = os.path.abspath(os.path.dirname(__file__))
+    # Get fluxmap
+    fits_path = Path(here, 'data', 'defocus4_master.fits') # 'sci_fluxmap.fits')
+    master_fluxmap = fits.getdata(fits_path).astype(float)  # (photons/pix/s for 0 mag)
+    mag = 4 # Magnitude of star we're actually using
+    fluxmap = master_fluxmap * 10**(-0.4*mag)
+    # Put fluxmap in 1024x1024 image section
+    full_fluxmap = np.zeros((1024, 1024)).astype(float)
+    full_fluxmap[0:fluxmap.shape[0], 0:fluxmap.shape[1]] = fluxmap
+    # master_defocus = 
+    # For the simplest possible use of EMCCDDetect, use its defaults
+    emccd = EMCCDDetect()
+
+    # If you are using Python<=3.9, you can also apply CTI to the frame.  If
+    # you have Python>3.9, this will not work if you are using the arcticpy
+    # installation that was included with this emccd_detect package. Below is
+    # how you could apply CTI.
+    # See (<https://github.com/jkeger/arcticpy/tree/row_wise/arcticpy>) for
+    # details on the optional inputs to add_cti() so that you can specify
+    # something meaningful for the EMCCD you have in mind.
+    # (using "try" so that this script still runs in the case that arcticpy
+    # is not viable.  In that case, running this method update_cti()
+    # will not work.)
+    try:
+        emccd.update_cti()
+    except:
+        pass
     for frametime in exposureTimes:
         for iframe in range(1,numFrames+1):
-            # Set up some inputs here
-            here = os.path.abspath(os.path.dirname(__file__))
-            # Get fluxmap
-            fits_path = Path(here, 'data', 'ramp_frame.fits') # 'sci_fluxmap.fits')
-            fluxmap = fits.getdata(fits_path).astype(float)  # (photons/pix/s)
-            # Put fluxmap in 1024x1024 image section
-            full_fluxmap = np.zeros((1024, 1024)).astype(float)
-            full_fluxmap[0:fluxmap.shape[0], 0:fluxmap.shape[1]] = fluxmap
-            # Specify frametime
-            frametime = exposureTimes  # s
-            # # Load CSV of nonlinearities
-            # csv_name = "nonlin_array.csv"
-            # # Prepare nonlinearity data to be interpolated
-            # nonlin_df = pd.read_csv(csv_name)
-            # nonlin_emgains = np.array(nonlin_df.columns[1:])
-            # nonlin_DNs = np.array(nonlin_df.iloc[1:,0])
-            # nonlin_vals = np.float64(nonlin_df.iloc[1:,1:])
-            
-        
-            # For the simplest possible use of EMCCDDetect, use its defaults
-            emccd = EMCCDDetect()
-        
-            # If you are using Python<=3.9, you can also apply CTI to the frame.  If
-            # you have Python>3.9, this will not work if you are using the arcticpy
-            # installation that was included with this emccd_detect package. Below is
-            # how you could apply CTI.
-            # See (<https://github.com/jkeger/arcticpy/tree/row_wise/arcticpy>) for
-            # details on the optional inputs to add_cti() so that you can specify
-            # something meaningful for the EMCCD you have in mind.
-            # (using "try" so that this script still runs in the case that arcticpy
-            # is not viable.  In that case, running this method update_cti()
-            # will not work.)
-            try:
-                emccd.update_cti()
-            except:
-                pass
             # Simulate only the fluxmap
             sim_sub_frame = emccd.sim_sub_frame(fluxmap, frametime)
             # Simulate the full frame (surround the full fluxmap with prescan, etc.)
@@ -114,25 +93,24 @@ if __name__ == '__main__':
             except:
                 pass
         
-        
             # For more control, each of the following parameters can be specified
             # Custom metadata path, if the user wants to use a different metadata file
             meta_path = Path(here, 'emccd_detect', 'util', 'metadata.yaml')
             # Note that the defaults for full_well_serial and eperdn are specified in
             # the metadata file
-            em_gain=10.
+            em_gain=1.
             emccd = EMCCDDetect(
                 em_gain=em_gain,
                 full_well_image=60000.,  # e-
                 full_well_serial=100000.,  # e-
-                dark_current=0.0028,  # e-/pix/s
+                dark_current=.00028,  # e-/pix/s
                 cic=0.02,  # e-/pix/frame
-                read_noise=100.,  # e-/pix/frame
-                bias=10000.,  # e-
+                read_noise=140.,  # e-/pix/frame
+                bias=3000.,  # e-
                 qe=0.9,
                 cr_rate=0, #5.,  # hits/cm^2/s
                 pixel_pitch=13e-6,  # m
-                eperdn=7.,
+                eperdn=8.5,
                 nbits=14,
                 numel_gain_register=604,
                 meta_path=meta_path
@@ -150,7 +128,7 @@ if __name__ == '__main__':
             
             # Save Full Frame With Nonlinearity to FITS
             fitsfile = fits.HDUList([fits.PrimaryHDU(fullframe_x_nonlin)])
-            fitsfile.writeto('Nonlinearity'+str(iframe)+'.fits',overwrite=True)
+            fitsfile.writeto('./Output/Nonlinearity_'+str(frametime)+'s_'+str(iframe)+'.fits',overwrite=True)
         
             # # The class also has some convenience functions to help with inspecting the
             # # simulated frame
@@ -171,7 +149,7 @@ if __name__ == '__main__':
             # imagesc(subframe_x_nonlin, 'Output Sub Frame w Nonlinearity')
             # imagesc(subframe_nonlinearity, 'Nonlinearity of Sub Frame')
             # imagesc(sim_full_frame, 'Output Full Frame')
-            if iframe == 1;
+            if iframe == 1:
                 imagesc(fullframe_x_nonlin, 'Output Full Frame w Nonlinearity @ '+str(frametime)+'s exposure')
             # imagesc(fullframe_nonlinearity, 'Nonlinearity of Full Frame')
             # plt.show()
